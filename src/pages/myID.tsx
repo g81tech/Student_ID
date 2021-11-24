332af6ff2a9a5bb9379f25737c9fce3055bbf3d9
leksansilva <69767375+leksansilva@users.noreply.github.com>
2021-11-24 10:54:56 -0300

    Reajustes

---
+++ src/pages/myID.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import { useRouter } from "next/router";
+
+import Head from 'next/head'
+
+import * as C from '../styles/pages.Styles'
+
+
+const myID: NextPage = () => {
+
+    return(
+        <C.Container>
+        <Head>
+            <title>Carteirinha Estudantil | UNEB</title>
+        </Head>
+        
+        <h1>...Em Construção </h1>
+        </C.Container>
+    )
+}
+
+export default myID
