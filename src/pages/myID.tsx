34fa53ff6a8af42e966358fb6f155a6b8c3807b9
Gabriel Silva da Cruz <59829659+Gabrielg81@users.noreply.github.com>
2021-11-24 11:03:25 -0300

    Mudança de classe onClick da API

---
+++ src/pages/myID.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+
+import Head from 'next/head'
+
+import * as C from '../styles/pages.Styles'
+
+
+const myID: NextPage = () => {
+        return(
+        <C.Container>
+        <Head>
+            <title>Carteirinha Estudantil | UNEB</title>
+        </Head>
+        
+        <h1>Em desenvolvimento!</h1>
+        </C.Container>
+    )
+}
+
+export default myID
